31bacacc6cd3099a35a8a7730b94d9a514eb376f
Vincent Vandemeulebrouck <vincent.vandemeulebrouck@itiviti.com>
2021-12-09 02:18:39 +0100

    Adding more display
    - Skills / Knowledge
    - metatype
    - karma
    - nature (mundane/awakened/emerged)
    
    Change-Id: Ia5566f0b0feef653c6809b4b80bbf430058a4e4c

--- module/config.js
+++ module/config.js
@@ -2,38 +2,58 @@ export const SRA = {
     sheet: {
         actorName: "SRA.sheet.actorName",
         character: "SRA.sheet.character",
-        keywords: {
-            name: "SRA.sheet.keyword.name",
+        karma: "SRA.sheet.karma",
+        karmatotal: "SRA.sheet.karmatotal",
+        singular: {
+            keyword: "SRA.sheet.singular.keyword",
+            disposition: "SRA.sheet.singular.disposition",
+            cue: "SRA.sheet.singular.cue",
         },
-        dispositions: {
-            name: "SRA.sheet.dispositions.name",
-        },
-        cues: {
-            name: "SRA.sheet.cues.name",
+        plural: {
+            keyword: "SRA.sheet.plural.keyword",
+            disposition: "SRA.sheet.plural.disposition",
+            cue: "SRA.sheet.plural.cue",
         },
     },
     common: {
         newEntry: "SRA.common.newEntry",
         add: "SRA.common.add",
-        del: "SRA.common.del"
+        del: "SRA.common.del",
+        edit: "SRA.common.edit"
     },
-    itemTypes: {
-        metatype: 'SRA.itemTypes.metatype',
-        skill: 'SRA.itemTypes.skill',
-        quality: 'SRA.itemTypes.quality',
-        shadowamp: 'SRA.itemTypes.shadowAmp',
-        weapon: 'SRA.itemTypes.weapon',
-        gear: 'SRA.itemTypes.gear',
-        contact: 'SRA.itemTypes.contact'
+    itemType: {
+        singular: {
+            metatype: 'SRA.itemType.singular.metatype',
+            skill: 'SRA.itemType.singular.skill',
+            quality: 'SRA.itemType.singular.quality',
+            shadowamp: 'SRA.itemType.singular.shadowamp',
+            weapon: 'SRA.itemType.singular.weapon',
+            gear: 'SRA.itemType.singular.gear',
+            contact: 'SRA.itemType.singular.contact'
+        },
+        plural: {
+            metatype: 'SRA.itemType.plural.metatype',
+            skill: 'SRA.itemType.plural.skill',
+            quality: 'SRA.itemType.plural.quality',
+            shadowamp: 'SRA.itemType.plural.shadowamp',
+            weapon: 'SRA.itemType.plural.weapon',
+            gear: 'SRA.itemType.plural.gear',
+            contact: 'SRA.itemType.plural.contact'
+        },
     },
-
+    capacity: {
+        mundane: 'SRA.capacity.mundane',
+        awakened: 'SRA.capacity.awakened',
+        emerged: 'SRA.capacity.emerged'
+    },
     attributes: {
         strength: 'SRA.attributes.strength',
-        agility: 'SRA.attributes.Aaility',
+        agility: 'SRA.attributes.agility',
         willpower: 'SRA.attributes.willpower',
         logic: 'SRA.attributes.logic',
         charisma: 'SRA.attributes.charisma',
         edge: 'SRA.attributes.edge',
+        knowledge: 'SRA.attributes.knowledge',
     },
 
 };
--- module/handlebars-manager.js
+++ module/handlebars-manager.js
@@ -1,10 +1,16 @@
 import { TEMPLATES_PATH } from "./constants.js";
+import { Grammar } from "./grammar.js";
 import { Misc } from "./misc.js";
 
 export const partials = [
+  `${TEMPLATES_PATH}/actor/parts/metatype.hbs`,
+  `${TEMPLATES_PATH}/actor/parts/capacity.hbs`,
+  `${TEMPLATES_PATH}/actor/parts/karma.hbs`,
   `${TEMPLATES_PATH}/actor/parts/attributes.hbs`,
   `${TEMPLATES_PATH}/actor/parts/attribute.hbs`,
   `${TEMPLATES_PATH}/actor/parts/wordlist.hbs`,
+  `${TEMPLATES_PATH}/actor/parts/skills.hbs`,
+  `${TEMPLATES_PATH}/actor/parts/skill.hbs`,
 ];
 
 export class HandlebarsManager {
@@ -15,5 +21,7 @@ export class HandlebarsManager {
 
   static async register() {
     Handlebars.registerHelper('concat', (...args) => Misc.join(args.slice(0, -1)));
+    Handlebars.registerHelper('substring', (str, from, to) => str?.substring(from, to));
+    Handlebars.registerHelper('toUpperCase', (str) => Grammar.toUpperCaseNoAccent(str));
   }
 }
