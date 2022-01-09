d88413125dc014b2fda3f7b5fe085bf94cf349cf
Vincent Vandemeulebrouck <vincent.vandemeulebrouck@itiviti.com>
2022-01-09 23:59:39 +0100

    Separate item sheets

--- module/anarchy-system.js
+++ module/anarchy-system.js
@@ -1,28 +1,33 @@
 import { ANARCHY } from './config.js';
-import { AnarchyBaseItem } from './item/anarchy-base-item.js';
-import { AnarchyBaseActor } from './actor/base-actor.js';
-import { CharacterActor } from './actor/character-actor.js';
-import { BaseItemSheet } from './item/base-item-sheet.js';
-import { CharacterActorSheet } from './actor/character-actor-sheet.js';
-import { ChatManager } from './chat/chat-manager.js';
-import { DeviceActorSheet } from './actor/device-actor-sheet.js';
 import { Enums } from './enums.js';
+import { LOG_HEAD, SYSTEM_NAME } from './constants.js';
+import { ChatManager } from './chat/chat-manager.js';
 import { GMAnarchy } from './app/gm-anarchy.js';
 import { GMManager } from './app/gm-manager.js';
 import { HandlebarsManager } from './handlebars-manager.js';
-import { LOG_HEAD, SYSTEM_NAME } from './constants.js';
-import { NPCCharacterActorSheet } from './actor/npc-character-actor-sheet.js';
 import { RemoteCall } from './remotecall.js';
-import { SkillItemSheet } from './item/skill-item-sheet.js';
 import { Styles } from './styles.js';
 import { AnarchyUsers } from './users.js';
-import { VehicleActorSheet } from './actor/vehicle-actor-sheet.js';
-import { VehicleActor } from './actor/vehicle-actor.js';
-import { DeviceActor } from './actor/device-actor.js';
 import { HooksManager } from './hooks-manager.js';
 import { AnarchyDice } from './roll/dice.js';
 import { AnarchyRoll } from './roll/anarchy-roll.js';
 import { Migrations } from './migrations.js';
+import { AnarchyBaseItem } from './item/anarchy-base-item.js';
+import { AnarchyBaseActor } from './actor/base-actor.js';
+import { CharacterActor } from './actor/character-actor.js';
+import { DeviceActor } from './actor/device-actor.js';
+import { VehicleActor } from './actor/vehicle-actor.js';
+import { CharacterActorSheet } from './actor/character-actor-sheet.js';
+import { DeviceActorSheet } from './actor/device-actor-sheet.js';
+import { VehicleActorSheet } from './actor/vehicle-actor-sheet.js';
+import { NPCCharacterActorSheet } from './actor/npc-character-actor-sheet.js';
+import { ContactItemSheet } from './item/contact-item-sheet.js';
+import { GearItemSheet } from './item/gear-item-sheet.js';
+import { MetatypeItemSheet } from './item/metatype-item-sheet.js';
+import { QualityItemSheet } from './item/quality-item-sheet.js';
+import { ShadowampItemSheet } from './item/shadowamp-item-sheet.js';
+import { SkillItemSheet } from './item/skill-item-sheet.js';
+import { WeaponItemSheet } from './item/weapon-item-sheet.js';
 
 /* -------------------------------------------- */
 /*  Foundry VTT AnarchySystem Initialization    */
@@ -112,14 +117,13 @@ export class AnarchySystem {
 
   loadItemSheets() {
     Items.unregisterSheet('core', ItemSheet);
-    Items.registerSheet(SYSTEM_NAME, BaseItemSheet, {
-      types: ["metatype", "quality", "shadowamp", "weapon", "gear", "contact"],
-      makeDefault: true
-    });
-    Items.registerSheet(SYSTEM_NAME, SkillItemSheet, {
-      types: ["skill"],
-      makeDefault: true
-    });
+    Items.registerSheet(SYSTEM_NAME, ContactItemSheet, { types: ["contact"], makeDefault: true });
+    Items.registerSheet(SYSTEM_NAME, GearItemSheet, { types: ["gear"], makeDefault: true });
+    Items.registerSheet(SYSTEM_NAME, MetatypeItemSheet, { types: ["metatype"], makeDefault: true });
+    Items.registerSheet(SYSTEM_NAME, QualityItemSheet, { types: ["quality"], makeDefault: true });
+    Items.registerSheet(SYSTEM_NAME, ShadowampItemSheet, { types: ["shadowamp"], makeDefault: true });
+    Items.registerSheet(SYSTEM_NAME, SkillItemSheet, { types: ["skill"], makeDefault: true });
+    Items.registerSheet(SYSTEM_NAME, WeaponItemSheet, { types: ["weapon"], makeDefault: true });
   }
 
 }
